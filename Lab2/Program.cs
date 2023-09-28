4dfe7b9d96bd37949b7507d74f4af650ebe31b19
VotreSeriteur <maksimbudrevich@gmail.com>
2023-09-28 23:17:31 +0300

    LAB 2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -4,53 +4,91 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int field;
-        int catCurrentPosition, mouseCurrentPosition;
-        int catsNextMove, mouseNextMove;
-
-        string curString;
-
-        //
-        field = 20;
-        catCurrentPosition = 4;
-        mouseCurrentPosition = 11;
+       
+        int? catCurrentPosition = null, mouseCurrentPosition = null;
+        
+       
         bool gameIsContinious = true;
+        bool mouseCathed = false;
+        int? mouseCathedPosition = null;
+        int catDistance = 0, mouseDistance = 0;
         //
-        
-        
-        
-        while (gameIsContinious)
+        using (StreamReader sr = new("ChaseData.txt")) using (StreamWriter sw = new("PursuitLog.txt"))
         {
-            curString = ReadLine();
-            (string, int) player = (Regex.Split(curString, @"\s+")[0], Convert.ToInt32(Regex.Split(curString, @"\s+")[1]));
-
-            switch (player.Item1)
+            try
             {
-                case "P":
-                    OutCurPositions(catCurrentPosition,mouseCurrentPosition);
-                    break;
-                case "C":
-                    catCurrentPosition = (player.Item2 + catCurrentPosition) % field+1;
-                    Write(catCurrentPosition);
-                    break;
-                case "M":
-                    mouseCurrentPosition = (player.Item2 + mouseCurrentPosition) % field;
-                    break;
 
-                    default:
-                        continue;
+                int field = Convert.ToInt32(sr.ReadLine().Trim());
+                string curString;
+
+                sw.WriteLine($"Cat and Mouse\nCat Mouse  Distance\n-------------------");
+
+                while (gameIsContinious)
+                {
+                    curString = sr.ReadLine();
+                    if (curString == null) { break; }
+                    (string, int) player = (Regex.Split(curString, @"\s+")[0], Regex.Split(curString, @"\s+").Length > 1 ? Convert.ToInt32(Regex.Split(curString, @"\s+")[1]) : 0);
+
+                    switch (player.Item1)
+                    {
+                        case "P":
+                            OutCurPositions(sw, catCurrentPosition, mouseCurrentPosition);
+
+                            break;
+                        case "C":
+                            if (catCurrentPosition != null)
+                            {
+                                catDistance += Math.Abs(player.Item2);
+                                catCurrentPosition = player.Item2 < 0 ? (player.Item2 + catCurrentPosition + field) % field : (player.Item2 + catCurrentPosition) % field + (player.Item2 <= field ? 0 : 1);
+                            }
+                            else catCurrentPosition = player.Item2;
+                           
+                            WriteLine(catDistance);
+                            break;
+                        case "M":
+                            if (mouseCurrentPosition != null)
+                            {
+                                mouseDistance += Math.Abs(player.Item2);
+                                mouseCurrentPosition = player.Item2 < 0 ? (player.Item2 + mouseCurrentPosition + field) % field : (player.Item2 + mouseCurrentPosition) % field + (player.Item2 <= field ? 0 : 1);
+                            }
+                            else mouseCurrentPosition = player.Item2;
+                            break;
+
+                        default:
+                            continue;
+                    }
+                    
+                    if (mouseCurrentPosition == catCurrentPosition) { gameIsContinious = false; mouseCathed = true; mouseCathedPosition = mouseCurrentPosition; } 
+                
+                }
+
             }
+            
+            catch { }
+            
+            finally {
+                
+                sw.WriteLine("-------------------" + '\n');
+                sw.WriteLine("Distance traveled:   Mouse     Cat");
+                sw.WriteLine("                    " + '\t' + mouseDistance + "\t\t" + catDistance);
+                
+                if (mouseCathed)
+                {
+                    sw.Write("Mouse cought at:" + mouseCathedPosition);
+                }
+                else
+                {
+                    sw.Write("Mouse was not cought");
+                }
 
-
-
-
-        }
-
-
+            } }
     }
 
-    public static void OutCurPositions(int catCurrentPosition, int mouseCurrentPosition)
+    public static void OutCurPositions(StreamWriter sw, int? catCurrentPosition, int? mouseCurrentPosition)
     {
-    
+        sw.Write((catCurrentPosition == null ? "??" : catCurrentPosition.ToString()) + '\t');
+        sw.Write((mouseCurrentPosition == null ? "??" : mouseCurrentPosition.ToString()) + '\t');
+        sw.Write(((catCurrentPosition == null || mouseCurrentPosition == null)? "  " : Math.Abs(Convert.ToDecimal(catCurrentPosition - mouseCurrentPosition)).ToString()) + '\n');
+
     }
 }
